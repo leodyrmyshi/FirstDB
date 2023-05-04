33110e30893d1954d7b3d67f5c753ca469875378
Leo Dyrmyshi <116791297+leodyrmyshi@users.noreply.github.com>
2023-05-04 09:13:02 +0200

    Prima versione

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+
+
+Console.WriteLine("Hello World");
+
+SQLiteConnection cnl = new SQLiteConnection("chinook.db");
+var tblArtist = cnl.Query<Artist>("select * from artists");
+Console.WriteLine( $"In questa tabella ci sono {tblArtist.Count} record!");
+
+public class Artist
+{
+    public int ArtistId{get; set;}
+    public string Name{get; set;}
+}
